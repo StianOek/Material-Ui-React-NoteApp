e5a12efccb7598535c5948f7e58d6d4858bbfc85
StianOek <66445281+StianOek@users.noreply.github.com>
2021-09-18 00:12:11 +0200

    finalizing costume styles hook

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,10 +1,37 @@
 import React from 'react'
+import { Container } from '@mui/material'
+import { Typography } from '@mui/material'
+import { Button } from '@mui/material'
+import AdbOutlinedIcon from '@mui/icons-material/AdbOutlined';
+import { makeStyles } from '@mui/styles';
+
+
+const useStyles = makeStyles({
+    btn: {
+        fontSize: 60,
+        backgroundColor: 'red'
+    },
+    text: {
+        color: 'red'
+    }
+})
 
 function Create() {
+
+    const classes = useStyles();
+
     return (
-        <div>
-            Create new note
-        </div>
+        <Container>
+            <Typography  variant="h6" component="h2" gutterBottom color="textSecondary">
+                Ley down a note to remember
+            </Typography>
+
+            <Button  type="submit" color="primary" variant="outlined" startIcon={<AdbOutlinedIcon />}>
+                Create
+            </Button>
+            <br />
+            
+        </Container>
     )
 }
 
